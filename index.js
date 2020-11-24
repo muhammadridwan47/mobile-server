174894d887fb6659cc34c87549908c936beff58b
muhammadridwan47 <rolilaz47@gmail.com>
2020-11-24 23:36:21 +0700

    fixed

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const express = require("express");
 const bodyParser = require("body-parser");
 require("dotenv").config();
 const cors = require("cors");
-const midtransClient = require("midtrans-client");
+const db = require("./src/helper/db");
 
 const app = express();
+const server = require('http').createServer(app);
+// console.log('creacr'. server)
+const io = require('socket.io')(server);
 // const db = require("./src/helper/db");
 const AuthRoute = require("./src/routes/Auth");
 const UserRoute = require("./src/routes/User");
@@ -19,11 +22,42 @@ app.use("/images", express.static("images"));
 
 app.get("/", (req, res) => res.send("<h2> Success </h2>"));
 
+app.use('*',cors())
+
 app.use("/api/v1/auth", AuthRoute);
 app.use("/api/v1/user", UserRoute);
 app.use("/api/v1/transaction", TransactionRoute);
 app.use("/api/v1/topup", TopupRoute);
 
-app.listen(process.env.PORT, () => {
+// db.query(`SELECT balance FROM user WHERE id=11`, (err, res) => {
+//   console.log(res[0].balance)
+//   // socket.broadcast.to(userId).emit('get-data', chat)
+// });
+
+io.on('connection', (socket)=> {
+  // const userId = socket.handshake.query.userId
+  console.log('user connect')
+  // socket.join(userId)
+  socket.on('initial-user', (id) => {
+    if (id) {
+      socket.join(id)
+      console.log('datany adalah ini: ',id)
+      db.query(`SELECT balance FROM user WHERE id=${id}`, (err, res) => {
+        io.to(id).emit('get-data', res[0].balance)
+      });
+    }
+  })
+      // disconnect 
+    socket.on('disconnect',() => {
+        console.log("Users disconnect to socket or server");
+    })
+
+  })
+
+
+server.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
 });
+
+
+
